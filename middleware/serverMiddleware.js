4d8f4fbc233db9f7d1b4d26d8886a81abe77899a
Brian Ruff <thelonewolf198927@gmail.com>
2019-10-13 04:54:58 -0400

    error handling for default case, 500, 404s

--- middleware/serverMiddleware.js
+++ middleware/serverMiddleware.js
@@ -14,6 +14,34 @@ const middleWare = server => {
   server.use(helmet());
   server.use(morgan("dev"));
   server.use("/users", userRoutes);
+  // error handler middleware
+  server.use((err, req, res, next) => {
+    res.status(err.statusCode || 500);
+    switch (res.statusCode) {
+      case 500:
+        res
+          .json({
+            message: err.message,
+            status: res.statusCode,
+            info: "Server Error has Occurred"
+          })
+          .end();
+      case 404:
+        res
+          .json({
+            message: err.message,
+            status: res.statusCode,
+            info: "Resource or Resources were not Found"
+          })
+          .end();
+      default:
+        res.json({
+          message: err.message,
+          status: 500,
+          info: "Default Error Message"
+        });
+    }
+  });
 };
 
 module.exports = middleWare;
